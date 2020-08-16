3e7bc4f50e6498e83298b341aa168d3d13cc3bc5
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-08-16 19:04:14 -0400

    js for display toggle

---
+++ context_menu/js/contextmanager.js
@@ -0,0 +1,62 @@
+document.documentElement.setAttribute('data-theme', 'light')
+lightMode = () => {
+    document.documentElement.setAttribute('data-theme', 'light')
+}
+darkMode = () => {
+    document.documentElement.setAttribute('data-theme', 'dark')
+}
+// light mode dark mode toggle
+
+const toggleMenu = (div, command) => {
+    div.style.display = command === "show" ? "block" : "none";
+
+}
+
+const setPosition = (div, {top, left}) => {
+    div.style.left = `${left}px`;
+    div.style.top = `${top}px`;
+    hideMenus()
+    toggleMenu(div, "show");
+}
+
+
+const displayMenu = (selector, menu) => {
+    let toggle = document.getElementById(selector) 
+    let display = document.getElementById(menu) 
+    let offset = display.offsetWidth
+    // (toggle.offsetLeft + toggle.offsetWidth)- offset,
+    
+    toggle.addEventListener('click', e => {
+        // bool for whether or not the div placement is outside the viewport
+        let outsideViewport = (toggle.offsetLeft > (window.innerHeight || document.documentElement.clientWidth))
+        if (outsideViewport) {
+            let origin = {
+                left: (toggle.offsetLeft + toggle.offsetWidth)- offset,
+                top: toggle.offsetTop + 33,
+            };
+            setPosition(display, origin)
+        } else {
+            let origin = {
+                left: toggle.offsetLeft ,
+                top: toggle.offsetTop + 33,
+            };
+            setPosition(display, origin)
+        }  
+    })
+}
+
+const menus = {
+    "security": "security-menu",
+    "profile": "profile-menu",
+    "settings": "settings-menu",
+}
+for (const [key, value] of Object.entries(menus)) {
+    displayMenu(key, value)
+}
+const hideMenus = () => {
+    for (const [key, value] of Object.entries(menus)) {
+        div = document.getElementById(value)
+        toggleMenu(div, "hide")
+        
+    }
+}
