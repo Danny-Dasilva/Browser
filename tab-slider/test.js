320dbfdf0a03500a9676a482ab05ba849065ae1a
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-08-20 21:52:39 -0400

    attempt at slider

---
+++ tab-slider/test.js
@@ -0,0 +1,40 @@
+var view = $("#tslshow");
+var move = "100px";
+move1 = 100
+var sliderLimit = -750
+let test = document.getElementById('tslshow')
+
+
+
+function animatio(elem, move, direction) {
+    var left = parseInt(elem.style.left, 10) || 0
+    let end
+    if (direction === 'left'){
+        end = left + move 
+    } else {
+        end = left - move
+    }
+
+    elem.animate([
+        { left: `${left}px` }, 
+        { left: `${end}px` }, 
+      ], { 
+        duration: 200,
+      });
+    elem.style.left = `${end}px`
+}
+
+
+$("#rightArrow").click(function(){
+    console.log(view.css("left"))
+    animatio(test, move1, "right")
+    // var currentPosition = parseInt(view.css("left"));
+    // if (currentPosition >= sliderLimit) view.stop(false,true).animate({left:"-="+move},{ duration: 200})
+
+});
+
+$("#leftArrow").click(function(){
+    animatio(test, move1, "left")
+
+});
+
